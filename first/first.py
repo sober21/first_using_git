e68eb7f8b698a2f70f461a4836d9fffe5d9d484b
trezvy_papa <mix_mmg@mail.ru>
2023-07-16 13:14:14 +0300

    Функция plus добавлена и модуль first.py и импортирована в модуль twice.py

--- first/first.py
+++ first/first.py
@@ -1,6 +1,9 @@
 def multiplay(a: int, b: int) -> int:
     return a * b
 
+def plus(a,b):
+    return a+b
+
 
 if __name__ == '__main__':
     print(multiplay(10, 54))
--- two/twice.py
+++ two/twice.py
@@ -1,4 +1,5 @@
-from first.first import multiplay
+from first.first import multiplay, plus
 
 if __name__ == '__main__':
-    print(multiplay(34,56))
+    print(2*multiplay(34,56))
+    print(2*plus(2+4))
