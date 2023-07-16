3eb96af0aa67d3861e3d3a9abe3f246749445854
trezvy_papa <mix_mmg@mail.ru>
2023-07-16 13:08:45 +0300

    Добавил модуль twice.py В модуле first.py изменил название функции calc на multiplay и удалил функцию two_calc

--- first/first.py
+++ first/first.py
@@ -1,10 +1,6 @@
-def calc(a: int, b: int) -> int:
+def multiplay(a: int, b: int) -> int:
     return a * b
 
 
-def two_calc(a: int) -> int:
-    return a * 2
-
-
 if __name__ == '__main__':
-    print(two_calc(calc(10, 54)))
+    print(multiplay(10, 54))
---
+++ two/twice.py
@@ -0,0 +1,4 @@
+from first.first import multiplay
+
+if __name__ == '__main__':
+    print(multiplay(34,56))
