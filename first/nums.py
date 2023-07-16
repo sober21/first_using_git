cda6ea8665b53b765bbaeeb96c0bf163dcc88ee2
sober21 <106674251+sober21@users.noreply.github.com>
2023-07-16 14:11:57 +0300

    Update nums.py
    В модуль nums.py добавлена функция divide

--- first/nums.py
+++ first/nums.py
@@ -9,6 +9,9 @@ def plus(a: int, b: int) -> int:
 def minus(a: int, b: int) -> int:
     return a - b
 
+def divide(a:int, b:int)->int:
+    return a/b
+
 
 if __name__ == '__main__':
     print(multiplay(10, 54))
